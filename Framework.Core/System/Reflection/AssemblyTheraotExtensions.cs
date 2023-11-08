d0afb8ce269d50e3155faf9184a4938d7b9163d8
OwnageIsMagic <OwnageIsMagic@users.noreply.github.com>
2023-11-09 01:44:34 +0300

    TypeInfo wip

---
+++ Framework.Core/System/Reflection/AssemblyTheraotExtensions.cs
@@ -0,0 +1,11 @@
+﻿#if NETFRAMEWORK && !NET45_OR_GREATER
+using System.Collections.Generic;
+
+namespace System.Reflection;
+
+public static class AssemblyTheraotExtensions
+{
+    // TODO: TypeInfo
+    public static IEnumerable<Type> DefinedTypes(this Assembly @this) => @this.GetTypes();
+}
+#endif
---
+++ Framework.Core/System/Reflection/TypeInfoTheraotExtensions.cs
@@ -0,0 +1,20 @@
+﻿#if NETFRAMEWORK && !NET45_OR_GREATER
+
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace System.Reflection;
+
+// TODO: TypeInfo
+public static class TypeInfoTheraotExtensions
+{
+    public static Type AsType(this Type t) => t;
+
+    public static MethodInfo? GetDeclaredMethod(this Type t, string name) => t.GetMethod(name, BindingFlags.DeclaredOnly);
+    public static PropertyInfo? GetDeclaredProperty(this Type t, string name) => t.GetProperty(name, BindingFlags.DeclaredOnly);
+    public static IEnumerable<ConstructorInfo> DeclaredConstructors(this Type t) => t.GetConstructors(BindingFlags.DeclaredOnly);
+    public static IEnumerable<FieldInfo> DeclaredFields(this Type t) => t.GetFields(BindingFlags.DeclaredOnly);
+
+    public static Type CreateTypeInfo(this TypeBuilder b) => b.CreateType();
+}
+#endif
